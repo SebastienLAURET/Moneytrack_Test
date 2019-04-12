992ae5fb0d81a8b36e14490dfec23f50f5f506b5
sebastien.lauret@epitech.eu <sebastien.lauret@epitech.eu>
2019-04-12 12:56:30 +0200

    rename class payload mangament by payload

--- lib/moneytrack_test/payload.rb
+++ lib/moneytrack_test/payload.rb
@@ -1,6 +1,31 @@
 module MoneytrackTest
-  PAYLOAD = {
-    "hello" => "world",
-    "key" => "value"
-  }.freeze
+  class Payload
+     attr_accessor :payload, :serialized, :signature
+
+     def initialize(payload)
+       @payload = payload
+       self.make_signature!
+     end
+
+     def make_signature!()
+       serialize!()
+       sign!()
+     end
+
+     def serialize()
+       @payload.to_msgpack
+     end
+
+     def serialize!()
+       @serialized = self.serialize
+     end
+
+     def sign()
+        Digest::SHA256.hexdigest(@serialized)
+     end
+
+     def sign!()
+       @signature = self.sign
+     end
+  end
 end
