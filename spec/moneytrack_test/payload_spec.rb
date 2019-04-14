73fef8179c985aa56477a177b4cfa15cde334d1e
sebastien.lauret@epitech.eu <sebastien.lauret@epitech.eu>
2019-04-14 23:56:23 +0200

    change test

--- spec/moneytrack_test/payload_spec.rb
+++ spec/moneytrack_test/payload_spec.rb
@@ -1,17 +1,7 @@
+require "spec_helper"
+
 RSpec.describe Payload do
-  context "Creation first block" do
-    let(:data) { {
-    "hello" => "world",
-    "key" => "value"
-    } }
-
-    it "signature" do
-        payload = Payload.new data
-        expect(payload.signature).to eq "ca9edf6b92aa42a4e90f8d13f114936cf64156d1d54e00af931ae5e7a24cae28"
-    end
-  end
-
-  context "Creation a block" do
+  context "make the payload signature" do
     let(:data) { {
     "hello" => "world",
     "key" => "value"
